628a9be4269dcb160a13762de8194e2ce9162e33
Pramod Srinivasan <spmd92@gmail.com>
2015-05-15 11:16:16 +0530

    First commit.

---
+++ 2_3a_knapSackProblem.cpp
@@ -0,0 +1,57 @@
+// The usual Hashtags
+#include<new>
+#include<cstdio>
+#include<cctype>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<sstream>
+#include<iostream>
+#include<strstream>
+
+// The Hashtags for Data Structure
+
+ #include<map>
+ #include<set>
+ #include<list>
+ #include<stack>
+ #include<deque>
+ #include<queue>
+ #include<vector>
+ #include<bitset>
+ #include<string>
+ #include<iterator>
+ #include<algorithm>
+
+ // Standard Namespace Inclusion
+ using namespace std;
+
+ int main()
+ {
+   ifstream myfile("knap.txt");
+   int sackSize,numItems;
+   long long value,weight;
+   myfile >> size >> numItems;
+   priority_queue< pair < long long, long long > > items;
+   while(scanf("%lld %lld ", &value, &weight) != -1)
+   {
+ items.push_back(make_pair(value,weight));
+   }
+   int knapSackMatrix[nItems+1][sackSize+1];
+   for(int i = 0; i <=  nItems ; i++)
+   {
+ for(int j = 0; j <= sackSize ; j++)
+ {
+if(i==0 || j == 0)
+  knapSackMatrix[i][j] = 0;
+else if(j - items[i].second  <= 0)
+  knapSackMatrix[i][j] = max(knapSackMatrix[i-1][j],knapSackMatrix[i-1][j - items[i].second] + items[i].first);
+
+else
+  knapSackMatrix[i][j] = knapSackMatrix[i-1][j];
+ }
+   }
+  cout <<"The optimal value for the knapsack problem is : " << knapsackMatrix[nItems][sackSize] << endl;
+  return 0;
+ }
+
