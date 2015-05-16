f2e06b6fba0d07b749f128a36151a4997987a30b
Pramod Srinivasan <spmd92@gmail.com>
2015-05-16 21:19:00 +0530

    Update 2_3a_knapSackProblem.cpp

--- 2_3a_knapSackProblem.cpp
+++ 2_3a_knapSackProblem.cpp
@@ -1,3 +1,13 @@
+/*
+In this programming problem and the next you'll code up the knapsack algorithm from lecture. Let's start with a warm-up. Download the text file here. This file describes a knapsack instance, and it has the following format:
+[knapsack_size][number_of_items]
+[value_1] [weight_1]
+[value_2] [weight_2]
+...
+For example, the third line of the file is "50074 659", indicating that the second item has value 50074 and size 659, respectively.
+
+*/
+
 // The usual Hashtags
 #include<new>
 #include<cstdio>
@@ -10,7 +20,6 @@
 #include<strstream>
 
 // The Hashtags for Data Structure
-
  #include<map>
  #include<set>
  #include<list>
@@ -29,28 +38,26 @@
  int main()
  {
    ifstream myfile("knap.txt");
-   int sackSize,numItems;
+   int sackSize,nItems;
    long long value,weight;
    myfile >> size >> numItems;
    priority_queue< pair < long long, long long > > items;
    while(scanf("%lld %lld ", &value, &weight) != -1)
    {
- items.push_back(make_pair(value,weight));
+      items.push_back(make_pair(value,weight));
    }
    int knapSackMatrix[nItems+1][sackSize+1];
    for(int i = 0; i <=  nItems ; i++)
    {
- for(int j = 0; j <= sackSize ; j++)
- {
-if(i==0 || j == 0)
-  knapSackMatrix[i][j] = 0;
-else if(j - items[i].second  <= 0)
-  knapSackMatrix[i][j] = max(knapSackMatrix[i-1][j],knapSackMatrix[i-1][j - items[i].second] + items[i].first);
-
-else
-  knapSackMatrix[i][j] = knapSackMatrix[i-1][j];
- }
-   }
+    for(int j = 0; j <= sackSize ; j++)
+    {
+      if(i==0 || j == 0)
+                knapSackMatrix[i][j] = 0;
+      else if(j - items[i].second  <= 0)
+                knapSackMatrix[i][j] = max(knapSackMatrix[i-1][j],knapSackMatrix[i-1][j - items[i].second] + items[i].first);
+      else      knapSackMatrix[i][j] = knapSackMatrix[i-1][j];
+    }
+  }
   cout <<"The optimal value for the knapsack problem is : " << knapsackMatrix[nItems][sackSize] << endl;
   return 0;
  }
